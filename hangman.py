fba06475f2de5c701dffb79122f46c5c83fa2e46
Revanth Kausikan <revanth.k.h@gmail.com>
2018-03-04 11:43:21 +0530

    Update hangman.py
    Removed 'time' import, to make it even more simpler

--- hangman.py
+++ hangman.py
@@ -1,18 +1,9 @@
-#importing the time module
-import time
-
 #welcoming the user
 name = raw_input("What is your name? ")
 
 print "Hello, " + name, "Time to play hangman!"
 
 print " "
-
-#wait for 1 second
-time.sleep(1)
-
-print "Start guessing..."
-time.sleep(0.5)
 
 #here we set the secret
 word = "secret"
